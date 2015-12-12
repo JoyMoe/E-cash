0251a5a7364018f23c919eef3ce027cd91ad70d1
7IN0SAN9 <me@7in0.me>
2015-12-12 22:06:49 +0800

    Support Safari

--- public/js/payment.js
+++ public/js/payment.js
@@ -4,7 +4,11 @@ $(function() ***REMOVED***
             gateway: $('input[name="gateway"]:checked').val()
         ***REMOVED***, function(result) ***REMOVED***
             var pay = window.open('about:blank', 'popUpWindow','height=750, width=1000, left=300, top=100, resizable=yes, scrollbars=yes, toolbar=yes, menubar=no, location=no, directories=no, status=yes'***REMOVED***
-            pay.document.write(result***REMOVED***
+            if (pay) ***REMOVED***
+                pay.document.write(result***REMOVED***
+            ***REMOVED*** else ***REMOVED***
+                document.write(result***REMOVED***
+            ***REMOVED***
         ***REMOVED***, 'html'***REMOVED***
 
         return false;
