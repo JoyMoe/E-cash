ab06016f50d680fe1b493565d924d7d84f94ae65
7IN0SAN9 <me@7in0.me>
2016-05-07 16:07:42 +0800

    fillable

--- app/Order.php
+++ app/Order.php
@@ -11,6 +11,8 @@ class Order extends Model
 
     protected $dates = ['deleted_at'];
 
+    protected $fillable = ['merchandiser_id', 'trade_no', 'subject', 'amount', 'items', 'returnUrl', 'notifyUrl'];
+
     public function merchandiser()
     {
         return $this->belongsTo('App\Merchandiser');
